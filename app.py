7da6b25bd5259cf89f142a555d4f4e63772aab6e
Art <absolutus11@gmail.com>
2023-09-29 10:21:27 +0400

    feat: Task 2 complete

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import os
 
 from flask import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 
 from src import auth, blog, db
 from src.models.models import User, Post, Group, session_db
 from flask_admin import Admin
+from flask_admin.form import SecureForm
 from flask_admin.contrib.sqla import ModelView
 
 from flask_bootstrap import Bootstrap5
+
+from flask_wtf import FlaskForm
+from wtforms import SelectField, DateField, SelectMultipleField, TimeField
+from wtforms.validators import DataRequired, Length
 
 
 def create_app(test_config=None):
@@ -28,54 +33,129 @@ def create_app(test_config=None):
     app.config["SECRET_KEY"] = "your_secret_key_here"
 
     app.config["BOOTSTRAP_BOOTSWATCH_THEME"] = "slate"
+    app.config['FLASK_ADMIN_SWATCH'] = 'flatly'
 
-    bootstrap = Bootstrap5(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
     
     db.init_app(app)
 
-    app.config["IPYTHON_CONFIG"] = {
-        "InteractiveShell": {
-            "colors": "Linux",
-            "confirm_exit": False,
-        },
-    }
+    bootstrap = Bootstrap5(app)
+    admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
+
+    # app.config["IPYTHON_CONFIG"] = {
+    #     "InteractiveShell": {
+    #         "colors": "Linux",
+    #         "confirm_exit": False,
+    #     },
+    # }
+
+    # **************
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    app.register_blueprint(auth.bp)
-
-    app.register_blueprint(blog.bp)
+    # **************
+    
 
     @app.route("/hello")
     def hello():
         return "Hello, World!"
 
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+    class UserForm(FlaskForm):
+        name = StringField('Name', [Length(min=2, max=128), DataRequired()])
+        password = PasswordField('Password', validators=[Length(min=3, max=128), DataRequired()])
+        admin = SelectField('Role', choices=["user", "admin"], validators=[DataRequired()])
+        # posts = StringField('Posts', validators=[DataRequired()])
+        # groups = SelectMultipleField('Groups', choices=["group1", "group2", "group3", "group4", "group5", "group6"], validators=[DataRequired()])
+        # favorites = StringField('Favorites', validators=[DataRequired()])
+        # comments = StringField('Comments')
+        # new_comments = StringField('New Comments', validators=[DataRequired()])
+        # created = DateField('Created', validators=[DataRequired()])
+        # updated = DateField('Updated', validators=[DataRequired()])
 
 
-    admin = Admin(app, name='microblog', template_mode='bootstrap3')
+    class UserModelView(ModelView):
+        # form_base_class = SecureForm
+        can_create = False
+        can_delete = False
+        # column_list = ['id', 'name', 'email', 'password', 'admin', 'role', 'groups', 'created']
+        # form_columns = ('name', 'email', 'password', 'admin', 'role', 'groups')
+        column_list = ['id', 'name', 'email', 'read', 'write', 'admin', 'groups', 'favorites', 'created']
+        form_columns = ['name', 'email', 'password', 'read', 'write', 'admin', 'groups', 'favorites']
+        # form_args = dict(
+        #         role=dict(label='Role', choices=['User', 'Admin'],  validators=[DataRequired()])
+        #     )
+        
+        # form = UserForm
 
-    from flask_wtf import FlaskForm
-    from wtforms import SelectField, DateField
-    from wtforms.validators import DataRequired
+        # column_choices = {
+        #         'my_column': [
+        #             ('db_value', 'display_value'),
+        #         ]
+        #     }
+
 
     class PostForm(FlaskForm):
-
         title = StringField('Title', validators=[DataRequired()])
         body = StringField('Body', validators=[DataRequired()])
-        user_id = SelectField('User', choices=[1,2,3], validators=[DataRequired()])
+        # user_id = SelectField('User', choices=get_users, validators=[DataRequired()])
+
+
+    # class PostForm(FlaskForm):
+        
+    #     @staticmethod
+    #     def get_users():
+    #         with app.app_context():
+    #             with session_db() as session:
+    #                 return [(str(user.id), user.name) for user in session.query(User).all()]
+        
+    #     title = StringField('Title', validators=[DataRequired()])
+    #     body = StringField('Body', validators=[DataRequired()])
+    #     user_id = SelectField('User', choices=get_users, validators=[DataRequired()])
+    #     created = DateField('Created', validators=[DataRequired()])
+
+
+    class PostModelView(ModelView):
+        can_edit = False
+        can_create = False
+        page_size = 20
+        column_list = ['id', 'title', 'body', 'user', 'likes', 'created']
+        form_columns = ['title', 'body', 'user']
+        # form = PostForm  # Use the custom form class
+
+
+    class GroupForm(FlaskForm):
+        
+        @staticmethod
+        def get_users():
+            with app.app_context():
+                with session_db() as session:
+                    return [(str(user.id), user.name) for user in session.query(User).all()]
+        
+        name = StringField('Name', validators=[DataRequired()])
+        users = SelectMultipleField('Users', choices=get_users, validators=[DataRequired()])
         created = DateField('Created', validators=[DataRequired()])
 
-    class PostAdminView(ModelView):
-        column_list = ['id', 'title', 'body', 'user_id', 'created']
-        form = PostForm  # Use the custom form class
+
+    class GroupModelView(ModelView):
+        # can_edit = False
+        # can_create = False
+        page_size = 20
+        column_list = ['id', 'name', 'users', 'read', 'write', 'created']
+        form_columns = ['name', 'users']
+        # form = GroupForm
 
 
-    admin.add_view(PostAdminView(Post, db.session))
-    admin.add_view(ModelView(User, db.session))
+
+
+    admin.add_view(PostModelView(Post, db.session))
+    admin.add_view(UserModelView(User, db.session))
+    # admin.add_view(ModelView(User, db.session))
+    # admin.add_view(ModelView(Group, db.session))
+    admin.add_view(GroupModelView(Group, db.session))
 
 
     return app
--- src/models/models.py
+++ src/models/models.py
@@ -31,59 +31,106 @@ favorites_likes_association_table = db.Table("favorites_likes_association_table"
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True)  
     password = db.Column(db.String(128))
-    role = db.Column(db.String(24))
-    posts = db.relationship("Post", backref="user", lazy=True)
     groups = db.relationship("Group", secondary=users_groups_association_table, back_populates="users")
+    posts = db.relationship("Post", back_populates="user")
+    read = db.Column(db.Boolean, default=True)
+    write = db.Column(db.Boolean, default=True)
+    admin = db.Column(db.Boolean, default=False)
     favorites = db.relationship("Post", secondary=favorites_likes_association_table, back_populates="likes")
-    comments = db.Column(db.String(128))
-    new_comments = db.Column(db.String(128))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    
+    role = db.Column(db.String(24))
 
-    def add_user_to_group(self, group):
-        try:
-            if group not in self.groups:
-                self.group.append(group)
-                db.session.commit()
-        except SQLAlchemyError as e:
-            print(e)
-
-    def remove_user_from_group(self, group):
-        try:
-            if group in self.groups:
-                self.groups.remove(group)
-                db.session.commit()
-        except SQLAlchemyError as e:
-            print(e)
+    def __str__(self):
+            return self.name
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
-    body = db.Column(db.String(255))
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey("group.id"), nullable=False)
+    body = db.Column(db.Text)
+    user_id = db.Column(db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", back_populates="posts")
     likes = db.relationship("User", secondary=favorites_likes_association_table, back_populates="favorites")
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __str__(self):
+        return self.title
+    
+    def like(self, user):
+        users = self.likes
+        print(users)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(128), unique=True)
+#     email = db.Column(db.String(128), unique=True)
+#     password = db.Column(db.String(128))
+#     role = db.Column(db.String(24))
+#     # posts = db.relationship("Post", backref="user", lazy=True)
+#     posts = db.relationship("Post", back_populates="user")
+#     groups = db.relationship("Group", secondary=users_groups_association_table, back_populates="users")
+#     favorites = db.relationship("Post", secondary=favorites_likes_association_table, back_populates="likes")
+#     comments = db.Column(db.String(128)) # ref to Comment model
+#     created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+#     # updated = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+#     # def __str__(self):
+#     #         return self.name
+
+#     def add_user_to_group(self, group):
+#         try:
+#             if group not in self.groups:
+#                 self.group.append(group)
+#                 db.session.commit()
+#         except SQLAlchemyError as e:
+#             print(e)
+
+#     def remove_user_from_group(self, group):
+#         try:
+#             if group in self.groups:
+#                 self.groups.remove(group)
+#                 db.session.commit()
+#         except SQLAlchemyError as e:
+#             print(e)
+
+# backref="user" = user field automatically added to represent creator of the post
+# class Post(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(255))
+#     body = db.Column(db.Text)
+#     user_id = db.Column(db.ForeignKey("user.id"), nullable=False)
+#     user = db.relationship("User", back_populates="posts")
+#     likes = db.relationship("User", secondary=favorites_likes_association_table, back_populates="favorites")
+#     created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+#     # updated = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+
 
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     users = db.relationship("User", secondary=users_groups_association_table, back_populates="groups")
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    read = db.Column(db.Boolean, default=True)
+    write = db.Column(db.Boolean, default=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    # # updated = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
-    def list_groups(self):
-        ...
-    
-    def add_group(self, data):
-        ...
+    def __str__(self):
+            return self.name
 
-    def edit_group(self, group, data):
-        ...
+    # def list_groups(self):
+    #     ...
     
-    def remove_group(self, group):
-        ...
+    # def add_group(self, data):
+    #     ...
+
+    # def edit_group(self, group, data):
+    #     ...
+    
+    # def remove_group(self, group):
+    #     ...
